1cca0f01667ee2cb1f5b04360e370b2ef9221d4f
SartSilver25 <lyonel_ag@outlook.fr>
2023-10-28 13:05:19 +0200

    btp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import SimpleSidebar from '@/components/sidebar'
 import Head from 'next/head'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <SimpleSidebar></SimpleSidebar>
       </main>
     </>
   )
